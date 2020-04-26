de4023536c54ad2442d3710ee71edd0fbcc2839a
Sarah <smhultin@rams.colostate.edu>
2020-04-25 20:32:48 -0600

    sarah/westin work session; changed a few cosmetic things

--- zombiedapp/src/pages/Greeting.js
+++ zombiedapp/src/pages/Greeting.js
@@ -14,18 +14,14 @@ class Greeting extends Component {
         <br />
         <h2 style={{ color: "DarkRed", textAlign: "center" }}>
           {" "}
-          Welcome to the CSU <b> CryptoZombies</b> game!
+          Welcome to our <b> Bitchin Blockchain of Custody</b>!
         </h2>
         <br />
-        <img src="static/images/zombieLoser2.jpg" style={imgStyle} width="400px" alt="Zombie rising from grave" />
+        <img src="static/images/net_forensics.jpg" style={imgStyle} width="400px" alt="Neato supremo" />
         <br /> <br />
         <p style={{ textAlign: "center" }}>
-          This CS481A3 blockchain game allows you to create one original zombie
-          on the Ethereum blockchain.
-          <br /> Once created, your zombie can eat cryptokitties to create new
-          zombies.
-          <br /> Additionally, your zombie can attack other zombies (you have a
-          70% chance of winning).
+          This CS481A3 blockchain project allows you to interact with a digital Chain of Custody on the Ethereum blockchain.
+          <br /> Go get some hoes!
           <br /> <br /> To get started, select a button from the menu bar above.
         </p>
       </div>
