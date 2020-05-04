15cde147f962e7238c01361a1dfc774cdbc002c5
Westin Musser <wstnmssr@gmail.com>
2020-05-03 22:27:15 -0600

    almost cool pics

--- zombiedapp/src/components/evidenceCardContent.js
+++ zombiedapp/src/components/evidenceCardContent.js
@@ -3,6 +3,13 @@ import {Card, Image} from "semantic-ui-react";
 //import ZombieChar from "./zombieChar";
 
 class EvidenceCardContent extends Component {
+   random_images_array = [
+    'static/images/evidence/broken hdd.jpg',
+    'static/images/evidence/broken phone.jpg',
+    'static/images/evidence/hacked phone.jpg',
+    'static/images/evidence/police-tape-laptop.jpg'
+  ]
+
   truncate = (text) => {
     if (text.length > 30) {
       var start = text.substring(0, 30);
@@ -14,7 +21,7 @@ class EvidenceCardContent extends Component {
   render() {
     return (
       <>
-        <Image src='static/images/france.jpg' />
+        <Image src= {this.random_images_array[3]}/>
         <Card.Content>
           <Card.Header>Evidence Description: </Card.Header>
           <Card.Description>
