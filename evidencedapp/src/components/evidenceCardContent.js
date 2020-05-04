af0b8d691988dec795f3c62cd6a93a5a7ecd6d0d
Westin Musser <wstnmssr@gmail.com>
2020-05-03 23:01:28 -0600

    different evidence image

--- evidencedapp/src/components/evidenceCardContent.js
+++ evidencedapp/src/components/evidenceCardContent.js
@@ -14,7 +14,7 @@ class EvidenceCardContent extends Component {
   render() {
     return (
       <>
-        <Image src='static/images/police-tape-laptop.jpg' />
+        <Image src='static/images/police-tape-laptop.png' />
         <Card.Content>
           <Card.Header>Evidence Description: </Card.Header>
           <Card.Description>
